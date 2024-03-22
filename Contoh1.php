226c82f34127a36e4d3be10658b9f34a37808919
sandipryowbw <164070148+sandipryowbw@users.noreply.github.com>
2024-03-22 14:26:43 +0700

    Add files via upload
    tugas pertemuan 2

---
+++ Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contoh1 extends CI_Controller {
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya sandi priyo w
+ Saya tinggal di daerah bekasis
+ olah raga yang saya sukai adalah
+ Bulutangkis";
+ }
+}
+
